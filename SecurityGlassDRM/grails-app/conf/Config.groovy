7c33a93286e1a8e377a26b920824513bbdaa6fda
ianibo <ianibbo@googlemail.com>
2012-07-13 15:56:05 +0100

    Installed spring security core and configured user and role domain classes

--- SecurityGlassDRM/grails-app/conf/Config.groovy
+++ SecurityGlassDRM/grails-app/conf/Config.groovy
@@ -89,3 +89,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.k_int.sgdrm.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.k_int.sgdrm.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.k_int.sgdrm.Role'
