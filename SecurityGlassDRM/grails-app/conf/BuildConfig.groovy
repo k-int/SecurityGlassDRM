3b5902d38a0d7b7c2f0ea952586ea86409940ba2
ianibo <ianibbo@googlemail.com>
2012-09-12 16:05:19 +0100

    Added extra dependency repository until corrupt grailsCentral repo is fixed

--- SecurityGlassDRM/grails-app/conf/BuildConfig.groovy
+++ SecurityGlassDRM/grails-app/conf/BuildConfig.groovy
@@ -12,11 +12,13 @@ grails.project.dependency.resolution = {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
+
+        mavenRepo "http://snapshots.repository.codehaus.org"
 
         grailsPlugins()
         grailsHome()
